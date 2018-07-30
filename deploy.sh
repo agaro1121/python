9a106cce3296e9d64b7d807319df94dc9db167b2
Hierro <hierro@localhost.localdomain>
2018-07-30 09:26:11 -0400

    makes deploy script more generic

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # cp ~/gitext/python/Github.py /home/hierro/.local/share/albert/org.albert.extension.python/modules/
-rm -rf /home/hierro/.local/share/albert/org.albert.extension.python/modules/Github
-cp -rf ~/gitext/python/Github/ /home/hierro/.local/share/albert/org.albert.extension.python/modules/
+rm -rf $HOME/.local/share/albert/org.albert.extension.python/modules/Github
+cp -rf ~/gitext/python/Github/ $HOME/.local/share/albert/org.albert.extension.python/modules/
